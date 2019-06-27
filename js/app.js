6bd027e950e3029a73e95a99cb4c4fc045f1e15d
Pimwipha Ch <pimwiphach.office@gmail.com>
2019-06-28 03:17:31 +0700

    add traffic and daily chart

---
+++ js/app.js
@@ -0,0 +1,136 @@
+// for a line chart
+
+var ctx = document.getElementById("trafficChart").getContext("2d");
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: {
+    labels: [
+      "16-22",
+      "23-29",
+      "30-5",
+      "6-12",
+      "13-19",
+      "20-26",
+      "27-3",
+      "4-10",
+      "11-17",
+      "18-24",
+      "25-31"
+    ],
+    datasets: [
+      {
+        label: "",
+        backgroundColor: "rgb(107, 112, 190, 0.2)",
+        borderColor: "rgb(107, 112, 190)",
+        borderWidth: "medium",
+        lineTension: 0,
+        pointRadius: 5,
+        pointBorderWidth: 2,
+        pointBackgroundColor: "#fff",
+        data: [
+          0,
+          750,
+          1250,
+          100,
+          1500,
+          2000,
+          1500,
+          1750,
+          1250,
+          1750,
+          2250,
+          1750,
+          2250
+        ]
+      }
+    ]
+  },
+  // Configuration options go here
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
+
+// for a bar chart
+
+var barChart = document.getElementById("dailyChart");
+var dailyChart = new Chart(barChart, {
+  type: "bar",
+  data: {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [
+      {
+        label: "# of Tomatoes",
+        data: [75, 100, 175, 125, 225, 200, 100],
+        backgroundColor: [
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)"
+        ],
+        borderColor: [
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)",
+          "rgba(107, 112, 190)"
+        ],
+        borderWidth: 1
+      }
+    ],
+    options: {
+      responsive: false,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  }
+});
+
+// for a doughnut chart
+
+var circleChart = document.getElementById("doughnutChart").getContext("2d");
+
+var doughnutChart = new Chart(circleChart, {
+  type: "doughnut",
+  data: {
+    labels: ["Phone", "Tablets", "Desktop"],
+    datasets: [
+      {
+        data: [15, 15, 70],
+        borderColor: ["#2196f38c", "#f443368c", "rgba(107, 112, 190)"],
+        backgroundColor: ["#2196f38c", "#f443368c", "rgba(107, 112, 190)"],
+        borderWidth: 1 // Specify bar border width
+      }
+    ]
+  },
+  options: {
+    responsive: true, // Instruct chart js to respond nicely.
+    maintainAspectRatio: false // Add to prevent default behaviour of full-width/height
+  }
+});
